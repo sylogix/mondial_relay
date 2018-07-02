a056a5885d388d2d5254a40727f46b197e2c838d
Žilvinas <zverseckas@gmail.com>
2018-07-02 10:40:30 +0300

    Remove backwards compatibility (#50)

--- lib/mondial_relay/configuration.rb
+++ lib/mondial_relay/configuration.rb
@@ -7,16 +7,13 @@ module MondialRelay
       drop_off_points
     ).freeze
 
-    attr_accessor :api_wsdl_url, :api_url, :merchant_id, :api_secret,
+    attr_accessor :api_url, :merchant_id, :api_secret,
                   :api_timeout, :api_max_retries, :debug, :monitor,
                   :enabled_services
 
     attr_reader :http_adapter
 
     def initialize
-      # TODO: This is not longer used. Remove it with the next major version
-      @api_wsdl_url = nil
-
       @api_url = 'http://www.mondialrelay.fr/webservice/'
 
       @merchant_id = nil
