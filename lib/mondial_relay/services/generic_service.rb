45e44ccc63baeb2945fea9f5c6443ceb1955023c
Florent Piteau <florent.piteau@sylogix.net>
2023-09-13 14:53:16 +0200

    MondialRelay is forcing https but did not update their WSDLs

--- lib/mondial_relay/services/generic_service.rb
+++ lib/mondial_relay/services/generic_service.rb
@@ -3,7 +3,8 @@
 module MondialRelay
   module Services
     class GenericService < BaseService
-      WSDL_URL = 'https://api.mondialrelay.com/Web_Services.asmx?WSDL'
+      #WSDL_URL = 'https://api.mondialrelay.com/Web_Services.asmx?WSDL'
+      WSDL_URL = File.join(File.dirname(__FILE__), 'wsdl/generic.wsdl')
 
       def initialize
         super(:generic)
--- lib/mondial_relay/services/parcel_shops_service.rb
+++ lib/mondial_relay/services/parcel_shops_service.rb
@@ -3,7 +3,8 @@
 module MondialRelay
   module Services
     class ParcelShopsService < BaseService
-      WSDL_URL = 'https://api-parcelshop.mondialrelay.com/Web_Services.asmx?WSDL'
+      #WSDL_URL = 'https://api-parcelshop.mondialrelay.com/Web_Services.asmx?WSDL'
+      WSDL_URL = File.join(File.dirname(__FILE__), 'wsdl/parcel_shops.wsdl')
 
       def initialize
         super(:parcel_shops)
